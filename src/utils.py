bb21da7645929520bc8df4d8c4b60bcc19dc60b7
Vadim Andreev <vadimcola@mail.ru>
2023-05-11 21:07:06 +0700

    созданы три функции getting_employer employer_id getting_vacancies

--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,35 @@ import json
 
 import requests as requests
 
-params = {'text': None,
-          'per_page': 100,
-          'only_with_vacancies': True
-          }
 
-response_url = requests.get('https://api.hh.ru/employers', params=params)
-response_data = json.loads(response_url.text)
-vacancies = response_data['items']
-for i in vacancies:
-    print(i)
+def getting_employer(keyword: str):
+    params = {'text': keyword,
+              'per_page': 10,
+              'only_with_vacancies': True
+              }
+
+    response_url = requests.get('https://api.hh.ru/employers', params=params)
+    response_data = json.loads(response_url.text)
+    employers = response_data['items']
+    return employers
+
+
+def employer_id(keyword):
+    employer_list = []
+    employers = getting_employer(keyword)
+    for emp in employers:
+        employer_list.append(emp['id'])
+    return tuple(employer_list)
+
+
+def getting_vacancies(index):
+    params = {'employer_id': index,
+              'per_page': 100,
+              }
+    response_url = requests.get('https://api.hh.ru/vacancies', params=params)
+    response_data = json.loads(response_url.text)
+    vacancies = response_data['items']
+    return vacancies
+
+
+
